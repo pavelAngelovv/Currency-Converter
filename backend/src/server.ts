7c011b61c2f8be981ed584330780d5100eb3d8df
Pavel Angelov <pangelov@codixis.com>
2025-02-23 11:36:11 +0200

    Add rate limiter to server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import rateLimit from "express-rate-limit";
 import currencyRoutes from "./routes/CurrencyRoutes.ts";
 import { updateCurrencies } from "./services/currencyService.ts";
 
@@ -9,6 +10,14 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: "You have reached the maximum number of requests within 15 minutes. Please try again later",
+});
+
+app.use(limiter);
 
 mongoose
   .connect(process.env.MONGODB_URI || "mongodb://localhost:27017/currency")
